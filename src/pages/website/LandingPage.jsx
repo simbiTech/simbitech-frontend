19a185733092348ec58b8ecf447d46a854c3bd1c
IwuchukwuDivine <ekeneifunanya@gmail.com>
2024-03-26 03:59:17 +0100

    feat: About Section

--- src/pages/website/LandingPage.jsx
+++ src/pages/website/LandingPage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import '../../styles/LandingPage.css'
 import NavBar from './Components/Navbar.jsx';
 import HeroSection from './Components/Hero.jsx';
+import About from './Components/About.jsx';
 
 
 export default function LandingPage () {
   return(
     <>
       <Header />
+      <About />
     </>
   );
 }
