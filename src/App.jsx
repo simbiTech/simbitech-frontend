44aba42e8184df23c2bb33345aebfa6512fdb407
Xcoder2023 <Xcoder2023@gmail.com>
2024-03-27 13:55:26 -0800

    signup page WIP

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import { useState } from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import LandingPage from './pages/website/LandingPage'
+import LogIn from './pages/website/onboardingPage/LogIn';
+import SignUp from './pages/website/onboardingPage/SignUp';
+import EmailVerification from './pages/website/onboardingPage/EmailVerification';
+import Forgetpwd from './pages/website/onboardingPage/Forgetpwd';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <Routes>
       <Route path='/' element={<LandingPage/>}/>
+      <Route path='/login' element={<LogIn/>}/>
+      <Route path='/forgetpwd' element={<Forgetpwd/>}/>
+      <Route path='/signup' element={<SignUp/>}/>
+      <Route path='/verify' element={<EmailVerification/>}/>
     </Routes>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import LogIn from './pages/website/onboardingPage/LogIn.jsx'
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* <App /> */}
-      <LogIn />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
