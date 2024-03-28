3273c63ed8bb5f8370c587cf8d02564a123f8c18
oreoluwa212 <105554286+oreoluwa212@users.noreply.github.com>
2024-03-28 12:26:40 +0100

    Revert "fontsize updated"

--- src/pages/website/onboardingPage/EmailVerification.jsx
+++ src/pages/website/onboardingPage/EmailVerification.jsx
@@ -11,7 +11,7 @@ const EmailVerification = () => {
   });
   return (
     <>
-      <div className="flex justify-center items-center h-[100vh] font-source-sans-pro text-[16px]">
+      <div className="text-100 flex justify-center items-center h-[100vh] font-source-sans-pro ">
         <div className="flex flex-col items-center gap-10 border-[1px] border-primarybase w-[90%] lg:w-[50%] p-5">
           <div className="flex items-center gap-2">
             <img src={brandlogo} alt="" />
@@ -24,7 +24,7 @@ const EmailVerification = () => {
             <img src={envelope} alt="" />
           </div>
           <div className="flex flex-col items-center">
-            <p className="opacity-70">We have sent a verification link to:</p>
+            <p className="opacity-50">We have sent a verification link to:</p>
             <p>{storeData}</p>
           </div>
           <p className="text-primarybase font-bold">Resend verification link</p>
